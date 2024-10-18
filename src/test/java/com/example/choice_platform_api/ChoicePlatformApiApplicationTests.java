97be3e1e1a28e945279c1c64dd0d9b0ef0922058
Jean <oka.jeanjaures@gmail.com>
2024-10-18 20:00:20 +0200

    feat : new simple controller

---
+++ src/test/java/com/example/choice_platform_api/ChoicePlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.choice_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoicePlatformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
