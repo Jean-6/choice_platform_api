e5b3d019ef7ebaec7ae2ec1cc652dbeb2fd41ac7
Jean <oka.jeanjaures@gmail.com>
2024-11-26 14:51:44 +0100

    refactor: add lombok annotations and rename field

--- src/main/java/com/example/choice_platform_api/entity/User.java
+++ src/main/java/com/example/choice_platform_api/entity/User.java
@@ -2,42 +2,35 @@ package com.example.choice_platform_api.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
-@Entity
+
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
+@Entity
 @Table(name = "users")
-@Slf4j
-public class User implements UserDetails {
-
-
+public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
-    private String login;
-    private String mdp;
-    private List<Role> roles ;
+    private String username;
+    private String password;
+    private boolean enabled;
+    @Enumerated(EnumType.STRING)
+    private Role role ;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
     }
-
-    @Override
-    public String getPassword() {
-        return "";
-    }
-
-    @Override
-    public String getUsername() {
-        return "";
-    }
-
 }
